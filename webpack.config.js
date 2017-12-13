4a1dc48e3ba6ca5edac29ff73d90ffc33330b6dd
Marei Kikukawa <contact@calmery.me>
2017-12-13 20:00:17 +0900

    Add base of backend

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,20 @@ const path    = require( 'path' )
 const webpack = require( 'webpack' )
 const merge   = require( 'webpack-merge' )
 
+const nodeExternals     = require( 'webpack-node-externals' )
 const MinifyPlugin      = require( 'babel-minify-webpack-plugin' )
 const CopyWebpackPlugin = require( 'copy-webpack-plugin' )
 
 const resolve = source => path.resolve( __dirname, source )
 
-const entryPath = resolve( 'src/entry.js' )
+const serverEntryPath = resolve( 'src/server.js' )
+const entryPath       = resolve( 'src/public/entry.js' )
 
 const common = {
   devtool: 'source-map',
   resolve: {
     extensions: ['.js']
   },
-  plugins: [new MinifyPlugin()],
   module: {
     loaders: [ {
       test   : /\.js$/,
@@ -25,6 +26,19 @@ const common = {
       }
     } ]
   }
+}
+
+const backend = {
+  entry: serverEntryPath,
+  output: {
+    path: resolve( './dist' ),
+    filename: 'server.js'
+  },
+  externals: [nodeExternals()],
+  node: {
+    __dirname: false
+  },
+  target: 'node'
 }
 
 const frontend = {
@@ -42,16 +56,18 @@ const frontend = {
   },
   plugins: [
     new CopyWebpackPlugin( [
-      { from: resolve( './src/index.html' ), to: resolve( './public/index.html' ) },
-      { from: resolve( './src/static/img' ), to: resolve( './dist/static/img' ) }
-    ] )
+      { from: resolve( './src/public/index.html' ), to: resolve( './dist/public/index.html' ) },
+      { from: resolve( './src/public/static/img' ), to: resolve( './dist/public/static/img' ) }
+    ] ),
+    new MinifyPlugin(),
   ],
   output: {
-    path: resolve( './public/' ),
+    path: resolve( './dist/public/' ),
     filename: 'porch.js'
   }
 }
 
 module.exports = [
+  merge( common, backend ),
   merge( common, frontend )
 ]
