f6a264c60aef7c6a2213187bf502e296277bd04f
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-12-15 19:39:03 +0900

    fix Procfile

--- chat/app.js
+++ chat/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 const socket = require('socket.io');
 
 const listen = app.listen(PORT, () => {
