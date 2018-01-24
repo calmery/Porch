ad668b9e3d97cd38eea0430b5d3d30ec5ce71e7c
Marei Kikukawa <contact@calmery.me>
2018-01-24 21:47:07 +0900

    Refactored src/public/entry.js

--- src/public/entry.js
+++ src/public/entry.js
@@ -12,60 +12,71 @@ import io from './static/js/socket.io.js'
 const socket = io()
 const app = Elm.Main.fullscreen()
 
+const log = console.log
+const dump = JSON.stringify
+
 app.ports.setTitle.subscribe( title =>
   document.title = title
 )
 
 socket.on( 'statuses/timeline', ( { data } ) => {
-  console.log( 'Server -> Client -> Elm : statuses/timeline' )
-  app.ports.statusesTimeline.send( JSON.stringify( data ) )
+  log( 'Server -> Client -> Elm : statuses/timeline' )
+  app.ports.statusesTimeline.send( dump( data ) )
 } )
+
 socket.on( 'statuses/update', ( { data } ) => {
-  console.log( 'Server -> Client -> Elm : statuses/update' )
-  app.ports.statusesUpdate.send( JSON.stringify( data ) )
+  log( 'Server -> Client -> Elm : statuses/update' )
+  app.ports.statusesUpdate.send( dump( data ) )
 } )
+
 app.ports.sendMessage.subscribe( text => {
-  console.log( 'Elm -> Client -> Server : statuses/update' )
+  log( 'Elm -> Client -> Server : statuses/update' )
   socket.emit( 'statuses/update', ( { text } ) )
 } )
 
 socket.on( 'users', ( { data } ) => {
-  console.log( 'Server -> Client -> Elm : users' )
-  app.ports.users.send( JSON.stringify( Object.values( data ) ) )
+  log( 'Server -> Client -> Elm : users' )
+  app.ports.users.send( dump( Object.values( data ) ) )
 } )
+
 socket.on( 'users/update', ( { data } ) => {
-  console.log( 'Server -> Client -> Elm : users/update' )
-  app.ports.usersUpdate.send( JSON.stringify( data ) )
+  log( 'Server -> Client -> Elm : users/update' )
+  app.ports.usersUpdate.send( dump( data ) )
 } )
 
 socket.on( 'account', ( { data } ) => {
-  console.log( 'Server -> Client -> Elm : account' )
-  app.ports.account.send( JSON.stringify( data ) )
+  log( 'Server -> Client -> Elm : account' )
+  app.ports.account.send( dump( data ) )
 } )
+
 socket.on( 'account/update_profile', response => {
-  console.log( 'Server -> Client -> Elm : account/update_profile' )
+  log( 'Server -> Client -> Elm : account/update_profile' )
   if( undefined !== response._ )
-    app.ports.account.send( JSON.stringify( response.data ) )
+    app.ports.account.send( dump( response.data ) )
   else
-    app.ports.accountUpdateProfile.send( JSON.stringify( response.data ) )
+    app.ports.accountUpdateProfile.send( dump( response.data ) )
 } )
+
 socket.on( 'account/update_profile_image', response => {
-  console.log( 'Server -> Client -> Elm : account/update_profile' )
+  log( 'Server -> Client -> Elm : account/update_profile' )
   if( undefined !== response._ )
-    app.ports.account.send( JSON.stringify( response.data ) )
+    app.ports.account.send( dump( response.data ) )
   else
-    app.ports.accountUpdateProfile.send( JSON.stringify( response.data ) )
+    app.ports.accountUpdateProfile.send( dump( response.data ) )
 } )
+
 app.ports.accountUpdateName.subscribe( name => {
-  console.log( 'Elm -> Client -> Server : account/update_profile' )
+  log( 'Elm -> Client -> Server : account/update_profile' )
   socket.emit( 'account/update_profile', ( { _: socket.id, name } ) )
 } )
+
 app.ports.accountUpdateScreenName.subscribe( screen_name => {
-  console.log( 'Elm -> Client -> Server : account/update_profile' )
+  log( 'Elm -> Client -> Server : account/update_profile' )
   socket.emit( 'account/update_profile', ( { _: socket.id, screen_name } ) )
 } )
+
 app.ports.accountUpdateImage.subscribe( _ => {
-  console.log( 'Elm -> Client -> Server : account/update_profile_image' )
+  log( 'Elm -> Client -> Server : account/update_profile_image' )
   const e = document.getElementById( 'image' )
   if( undefined !== e ){
     const image = e.files[0]
@@ -82,5 +93,3 @@ app.ports.accountUpdateImage.subscribe( _ => {
     }
   }
 } )
-
-window.socket = socket
